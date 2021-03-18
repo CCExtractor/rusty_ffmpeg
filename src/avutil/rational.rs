a1db797c47c8ff0712dcaefe5de94d1a2a06e2c6
ldm0 <ldm2993593805@163.com>
2021-03-18 05:46:40 +0000

    Add comments for rational apis

--- src/avutil/rational.rs
+++ src/avutil/rational.rs
@@ -3,10 +3,26 @@
 /// Ref: https://github.com/rust-lang/rust-bindgen/issues/1344
 use crate::ffi::AVRational;
 
+/// Create an AVRational.
+///
+/// Useful for compilers that do not support compound literals.
+///
+/// @note The return value is not reduced.
+/// @see av_reduce()
 pub fn av_make_q(num: libc::c_int, den: libc::c_int) -> AVRational {
     AVRational { num, den }
 }
 
+/// Compare two rationals.
+///
+/// @param a First rational
+/// @param b Second rational
+///
+/// @return One of the following values:
+///         - 0 if `a == b`
+///         - 1 if `a > b`
+///         - -1 if `a < b`
+///         - `INT_MIN` if one of the values is of the form `0 / 0`
 pub fn av_cmp_q(a: AVRational, b: AVRational) -> libc::c_int {
     let tmp = i64::from(a.num) * i64::from(b.den) - i64::from(b.num) * i64::from(a.den);
 
@@ -21,10 +37,17 @@ pub fn av_cmp_q(a: AVRational, b: AVRational) -> libc::c_int {
     }
 }
 
+/// Convert an AVRational to a `double`.
+/// @param a AVRational to convert
+/// @return `a` in floating-point form
+/// @see av_d2q()
 pub fn av_q2d(a: AVRational) -> libc::c_double {
     libc::c_double::from(a.num) / libc::c_double::from(a.den)
 }
 
+/// Invert a rational.
+/// @param q value
+/// @return 1 / q
 pub fn av_inv_q(q: AVRational) -> AVRational {
     AVRational {
         num: q.den,
