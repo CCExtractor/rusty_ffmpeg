284eade613dd2af0676850e5c5ff0052be8b5022
Donough Liu <liudingming@bytedance.com>
2022-06-20 00:51:07 +0800

    Bump version to 0.9.5 (#68)
    * Bump version to 0.9.5
    
    Use more `camino::Utf8Path`
    
    Bump rust-bindgen to 0.60
    
    Add verbatim path compatibility support
    
    * clippy fix

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use bindgen::{self, callbacks, Bindings};
 use once_cell::sync::Lazy;
 
-use std::{collections::HashSet, env, fs, path::PathBuf};
+use camino::Utf8Path as Path;
+use camino::Utf8PathBuf as PathBuf;
+use std::{collections::HashSet, env, fs};
 
 /// All the libs that FFmpeg has
 static LIBS: Lazy<[&str; 7]> = Lazy::new(|| {
@@ -17,7 +19,7 @@ static LIBS: Lazy<[&str; 7]> = Lazy::new(|| {
 });
 
 /// Whitelist of the headers we want to generate bindings
-static HEADERS: Lazy<[&str; 63]> = Lazy::new(|| {
+static HEADERS: Lazy<Vec<PathBuf>> = Lazy::new(|| {
     [
         "libavcodec/avcodec.h",
         "libavcodec/avfft.h",
@@ -83,6 +85,9 @@ static HEADERS: Lazy<[&str; 63]> = Lazy::new(|| {
         "libswresample/swresample.h",
         "libswscale/swscale.h",
     ]
+    .into_iter()
+    .map(|x| Path::new(x).into_iter().collect())
+    .collect()
 });
 
 /// Filter out all symbols in the HashSet, and for others things it will act
@@ -108,14 +113,11 @@ impl callbacks::ParseCallbacks for FilterCargoCallbacks {
     }
 }
 
-fn use_prebuilt_binding(from: &str, to: &str) {
+fn use_prebuilt_binding(from: &Path, to: &Path) {
     fs::copy(from, to).expect("Prebuilt binding file failed to be copied.");
 }
 
-fn generate_bindings<T: Into<String>>(
-    ffmpeg_include_dir: Option<&str>,
-    headers: impl Iterator<Item = T>,
-) -> Result<Bindings, ()> {
+fn generate_bindings(ffmpeg_include_dir: Option<&Path>, headers: &[PathBuf]) -> Bindings {
     // Because of the strange `FP_*` in `math.h` https://github.com/rust-lang/rust-bindgen/issues/687
     let filter_callback = FilterCargoCallbacks::new(
         vec![
@@ -131,12 +133,13 @@ fn generate_bindings<T: Into<String>>(
 
     // Bindgen the headers
     headers
+        .iter()
         // map header short path to full path
         .map(|header| {
             if let Some(ffmpeg_include_dir) = ffmpeg_include_dir {
-                format!("{}/{}", ffmpeg_include_dir, header.into())
+                ffmpeg_include_dir.join(header)
             } else {
-                header.into()
+                header.clone()
             }
         })
         .fold(
@@ -144,16 +147,17 @@ fn generate_bindings<T: Into<String>>(
                 bindgen::builder()
                     .parse_callbacks(Box::new(filter_callback))
                     // Add clang path, for `#include` header finding in bindgen process.
-                    .clang_arg(format!("-I{}/", ffmpeg_include_dir))
+                    .clang_arg(format!("-I{}", ffmpeg_include_dir))
             } else {
                 bindgen::builder().parse_callbacks(Box::new(filter_callback))
             },
             |builder, header| builder.header(header),
         )
         .generate()
+        .expect("Binding generation failed.")
 }
 
-fn static_linking_with_libs_dir(library_names: &[&str], ffmpeg_libs_dir: &str) {
+fn static_linking_with_libs_dir(library_names: &[&str], ffmpeg_libs_dir: &Path) {
     println!("cargo:rustc-link-search=native={}", ffmpeg_libs_dir);
     for library_name in library_names {
         println!("cargo:rustc-link-lib=static={}", library_name);
@@ -163,12 +167,12 @@ fn static_linking_with_libs_dir(library_names: &[&str], ffmpeg_libs_dir: &str) {
 #[allow(dead_code)]
 pub struct EnvVars {
     docs_rs: Option<String>,
-    out_dir: Option<String>,
-    ffmpeg_include_dir: Option<String>,
-    ffmpeg_dll_path: Option<String>,
-    ffmpeg_pkg_config_path: Option<String>,
-    ffmpeg_libs_dir: Option<String>,
-    ffmpeg_binding_path: Option<String>,
+    out_dir: Option<PathBuf>,
+    ffmpeg_include_dir: Option<PathBuf>,
+    ffmpeg_dll_path: Option<PathBuf>,
+    ffmpeg_pkg_config_path: Option<PathBuf>,
+    ffmpeg_libs_dir: Option<PathBuf>,
+    ffmpeg_binding_path: Option<PathBuf>,
 }
 
 impl EnvVars {
@@ -182,14 +186,24 @@ impl EnvVars {
         */
         Self {
             docs_rs: env::var("DOCS_RS").ok(),
-            out_dir: env::var("OUT_DIR").ok(),
-            ffmpeg_include_dir: env::var("FFMPEG_INCLUDE_DIR").ok(),
-            ffmpeg_dll_path: env::var("FFMPEG_DLL_PATH").ok(),
-            ffmpeg_pkg_config_path: env::var("FFMPEG_PKG_CONFIG_PATH").ok(),
-            ffmpeg_libs_dir: env::var("FFMPEG_LIBS_DIR").ok(),
-            ffmpeg_binding_path: env::var("FFMPEG_BINDING_PATH").ok(),
+            out_dir: env::var("OUT_DIR").ok().map(remove_verbatim),
+            ffmpeg_include_dir: env::var("FFMPEG_INCLUDE_DIR").ok().map(remove_verbatim),
+            ffmpeg_dll_path: env::var("FFMPEG_DLL_PATH").ok().map(remove_verbatim),
+            ffmpeg_pkg_config_path: env::var("FFMPEG_PKG_CONFIG_PATH").ok().map(remove_verbatim),
+            ffmpeg_libs_dir: env::var("FFMPEG_LIBS_DIR").ok().map(remove_verbatim),
+            ffmpeg_binding_path: env::var("FFMPEG_BINDING_PATH").ok().map(remove_verbatim),
         }
     }
+}
+
+/// clang doens't support -I{verbatim path} on windows, so we need to remove it if possible.
+fn remove_verbatim(path: String) -> PathBuf {
+    let path = if let Some(path) = path.strip_prefix(r#"\\?\"#) {
+        path.to_string()
+    } else {
+        path
+    };
+    PathBuf::from(path)
 }
 
 #[cfg(not(target_os = "windows"))]
@@ -214,8 +228,8 @@ mod non_windows {
 
     pub fn static_linking_with_pkg_config(
         library_names: &[&str],
-        ffmpeg_pkg_config_path: &str,
-    ) -> Vec<String> {
+        ffmpeg_pkg_config_path: &Path,
+    ) -> Vec<PathBuf> {
         env::set_var("PKG_CONFIG_PATH", ffmpeg_pkg_config_path);
         // TODO: if specific library is not enabled, we should not probe it. If we
         // want to implement this, we Should modify try_probe_system_ffmpeg() too.
@@ -233,20 +247,20 @@ mod non_windows {
                 paths.insert(new_path);
             }
         }
-        paths.into_iter().collect()
+        paths.into_iter().map(PathBuf::from).collect()
     }
 }
 
 #[cfg(target_os = "windows")]
 mod windows {
     use super::*;
-    pub fn static_linking_vcpkg(_env_vars: &EnvVars, _library_names: &[&str]) -> Vec<String> {
+    pub fn static_linking_vcpkg(_env_vars: &EnvVars, _library_names: &[&str]) -> Vec<PathBuf> {
         vcpkg::Config::new()
             .find_package("ffmpeg")
             .unwrap()
             .include_paths
             .into_iter()
-            .map(|x| x.to_str().unwrap().to_string())
+            .map(|x| PathBuf::from_path_buf(x).unwrap())
             .collect()
     }
 }
@@ -254,13 +268,13 @@ mod windows {
 fn dynamic_linking(env_vars: &EnvVars) {
     let ffmpeg_dll_path = env_vars.ffmpeg_dll_path.as_ref().unwrap();
 
-    let output_binding_path = &format!("{}/binding.rs", env_vars.out_dir.as_ref().unwrap());
+    let output_binding_path = &env_vars.out_dir.as_ref().unwrap().join("binding.rs");
 
     // Extract dll name and the dir the dll is in.
     let (ffmpeg_dll_name, ffmpeg_dll_dir) = {
         let mut ffmpeg_dll_path = PathBuf::from(ffmpeg_dll_path);
         // Without extension.
-        let ffmpeg_dll_filename = ffmpeg_dll_path.file_stem().unwrap().to_str().unwrap();
+        let ffmpeg_dll_filename = ffmpeg_dll_path.file_stem().unwrap();
         let ffmpeg_dll_name = if cfg!(target_os = "windows") {
             ffmpeg_dll_filename
         } else {
@@ -269,7 +283,7 @@ fn dynamic_linking(env_vars: &EnvVars) {
         .to_string();
         // Remove file name.
         ffmpeg_dll_path.pop();
-        let ffmpeg_dll_path = ffmpeg_dll_path.to_str().unwrap().to_string();
+        let ffmpeg_dll_path = ffmpeg_dll_path.to_string();
         (ffmpeg_dll_name, ffmpeg_dll_path)
     };
 
@@ -279,8 +293,7 @@ fn dynamic_linking(env_vars: &EnvVars) {
     if let Some(ffmpeg_binding_path) = env_vars.ffmpeg_binding_path.as_ref() {
         use_prebuilt_binding(ffmpeg_binding_path, output_binding_path);
     } else if let Some(ffmpeg_include_dir) = env_vars.ffmpeg_include_dir.as_ref() {
-        generate_bindings(Some(ffmpeg_include_dir), HEADERS.iter().cloned())
-            .expect("Binding generation failed.")
+        generate_bindings(Some(ffmpeg_include_dir), &HEADERS)
             // Is it correct to generate binding to one file? :-/
             .write_to_file(output_binding_path)
             .expect("Cannot write binding to file.");
@@ -290,7 +303,7 @@ fn dynamic_linking(env_vars: &EnvVars) {
 }
 
 fn static_linking(env_vars: &EnvVars) {
-    let output_binding_path = &format!("{}/binding.rs", env_vars.out_dir.as_ref().unwrap());
+    let output_binding_path = &env_vars.out_dir.as_ref().unwrap().join("binding.rs");
 
     #[cfg(not(target_os = "windows"))]
     {
@@ -303,13 +316,11 @@ fn static_linking(env_vars: &EnvVars) {
                 use_prebuilt_binding(ffmpeg_binding_path, output_binding_path);
             } else if let Some(ffmpeg_include_dir) = env_vars.ffmpeg_include_dir.as_ref() {
                 // If use ffmpeg_pkg_config_path with ffmpeg_include_dir, prefer using the user given dir rather than pkg_config_path.
-                generate_bindings(Some(ffmpeg_include_dir), HEADERS.iter().cloned())
-                    .expect("Binding generation failed.")
+                generate_bindings(Some(ffmpeg_include_dir), &HEADERS)
                     .write_to_file(output_binding_path)
                     .expect("Cannot write binding to file.");
             } else {
-                generate_bindings(Some(&include_paths[0]), HEADERS.iter().cloned())
-                    .expect("Binding generation failed.")
+                generate_bindings(Some(&include_paths[0]), &HEADERS)
                     .write_to_file(output_binding_path)
                     .expect("Cannot write binding to file.");
             }
@@ -318,8 +329,7 @@ fn static_linking(env_vars: &EnvVars) {
             if let Some(ffmpeg_binding_path) = env_vars.ffmpeg_binding_path.as_ref() {
                 use_prebuilt_binding(ffmpeg_binding_path, output_binding_path);
             } else if let Some(ffmpeg_include_dir) = env_vars.ffmpeg_include_dir.as_ref() {
-                generate_bindings(Some(ffmpeg_include_dir), HEADERS.iter().cloned())
-                    .expect("Binding generation failed.")
+                generate_bindings(Some(ffmpeg_include_dir), &HEADERS)
                     .write_to_file(output_binding_path)
                     .expect("Cannot write binding to file.");
             } else {
@@ -337,8 +347,7 @@ fn static_linking(env_vars: &EnvVars) {
             if let Some(ffmpeg_binding_path) = env_vars.ffmpeg_binding_path.as_ref() {
                 use_prebuilt_binding(ffmpeg_binding_path, output_binding_path);
             } else if let Some(ffmpeg_include_dir) = env_vars.ffmpeg_include_dir.as_ref() {
-                generate_bindings(Some(ffmpeg_include_dir), HEADERS.iter().cloned())
-                    .expect("Binding generation failed.")
+                generate_bindings(Some(ffmpeg_include_dir), &HEADERS)
                     .write_to_file(output_binding_path)
                     .expect("Cannot write binding to file.");
             } else {
@@ -349,8 +358,7 @@ fn static_linking(env_vars: &EnvVars) {
             if let Some(ffmpeg_binding_path) = env_vars.ffmpeg_binding_path.as_ref() {
                 use_prebuilt_binding(ffmpeg_binding_path, output_binding_path);
             } else {
-                generate_bindings(Some(&include_paths[0]), HEADERS.iter().cloned())
-                    .expect("Binding generation failed.")
+                generate_bindings(Some(&include_paths[0]), &HEADERS)
                     .write_to_file(output_binding_path)
                     .expect("Cannot write binding to file.");
             }
@@ -366,8 +374,8 @@ fn docs_rs_linking(env_vars: &EnvVars) {
     // 10MB, which is not enough to fit in FFmpeg source files. So the only
     // thing we can do is copying the locally generated binding files to the
     // `OUT_DIR`.
-    let binding_file_path = &format!("{}/binding.rs", env_vars.out_dir.as_ref().unwrap());
-    use_prebuilt_binding("src/binding.rs", binding_file_path);
+    let binding_file_path = &env_vars.out_dir.as_ref().unwrap().join("binding.rs");
+    use_prebuilt_binding(Path::new("src/binding.rs"), binding_file_path);
 }
 
 fn main() {
